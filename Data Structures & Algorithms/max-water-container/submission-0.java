a7bc5efac8615cbec740aaf9d6a88980ace079a7
Aditya <autobot268@gmail.com>
2026-04-13 14:28:45 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0 ; 
+        int j = height.length -  1; 
+
+        int maxArea = Integer.MIN_VALUE; 
+        while ( i < j) {
+
+            int area = Math.min(height[i], height[j]) * (j-i); 
+
+            maxArea = Math.max(area, maxArea); 
+
+            if(height[i] <= height[j]) i++; 
+            else j--; 
+        }
+
+        return maxArea; 
+    }
+}
